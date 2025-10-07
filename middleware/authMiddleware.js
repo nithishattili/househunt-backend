203d1462bbeb34224bb1db10e8d69e51525da843
nithishattili <attilinithish128@gmail.com>
2025-10-07 23:00:06 +0530

    Initial commit: backend setup

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log("🔐 Authorization Header:", authHeader); // <== add this
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log("🔐 Token received:", token); // <== add this
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded; // { userId: ... }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
